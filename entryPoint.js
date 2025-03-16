fdb9a0763dda821748dfd3465cd59b84921123c9
yaren-png <ykotovskii@gmail.com>
2025-03-16 14:43:16 +0100

    Update and rename entryPoint to entryPoint.js

---
+++ entryPoint.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Création d'un élément texte
+    const texte = document.createElement("p");
+    texte.textContent = "Bienvenue sur ma page blanche !";
+
+    // Ajout du texte dans le body
+    document.body.appendChild(texte);
+});
